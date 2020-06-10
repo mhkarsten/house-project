7489bfdfcb53515a5e61111b469bb8df0db71de6
Max <m.karsten10@gmail.cim>
2020-06-10 18:40:59 +0200

    remove old server files

--- server_old/house_model/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User, Message, Build, BuildLog
-
-admin.site.register([User, Message, Build, BuildLog])
-
--- server_old/house_model/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HouseModelConfig(AppConfig):
-    name = 'house_model'
